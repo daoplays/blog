5b22a9b6142866990aba64057a41ea4eaec5f762
Lindley Lentati <daoplays@outlook.com>
2024-06-09 16:59:15 +0100

    update video

--- pages/bonkathon2024/demo.tsx
+++ pages/bonkathon2024/demo.tsx
@@ -9,8 +9,10 @@ const Demo = () => {
         background: "linear-gradient(180deg, #292929 0%, #0B0B0B 100%)",
       }}
     >
-      <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
-
+      
+      <video controls={true} width="800" height="500" muted={false}>
+        <source src="https://github.com/daoplays/blog/raw/main/components/bonkathon/demo.mp4" type="video/mp4"/>
+      </video>
     </main>
   );
 };
