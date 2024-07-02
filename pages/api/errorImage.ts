310403a805ac40f7466214eebccf317a24a79399
Lindley Lentati <daoplays@outlook.com>
2024-07-02 23:06:12 +0100

    blue square

--- pages/api/errorImage.ts
+++ pages/api/errorImage.ts
@@ -20,12 +20,27 @@ export default async function handler(req, res) {
     return;
 }
 try{
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'image/png');
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Encoding, Accept-Encoding");
-    res.send(JSON.stringify({ done: 'Test!' }));
-  
+
+    const size = 200; // Size of the square in pixels
+    
+    const blueSquare = await sharp({
+      create: {
+        width: size,
+        height: size,
+        channels: 4,
+        background: { r: 0, g: 0, b: 255, alpha: 1 }
+      }
+    })
+    .png()
+    .toBuffer();
+
+    console.log('Blue square generated, size:', blueSquare.length);
+
+    res.send(blueSquare);
 
   } catch (error) {
     console.error('Error generating image:', error);
